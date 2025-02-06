74c23bf353f8aba25ad902a3ddfcda187b1fce9e
Manoel Lima <manoel.lima@manaus.am.gov.br>
2025-02-06 13:51:00 -0400

    feat: extract part of footer into Order component

--- src/index.js
+++ src/index.js
@@ -141,10 +141,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
@@ -153,6 +150,17 @@ function Footer() {
     </footer>
   );
   // return React.createElement("footer", null, "We're currently open!");
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00. Come visit us or order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
 }
 
 // root comes form the div's id in index.html file
